560fe08518dff1c137fe4326a7f0ecb3b770fd15
SinemKrc <104202283+SinemKrc@users.noreply.github.com>
2023-12-30 21:18:08 +0300

    Add files via upload

---
+++ Grup_31_/src/odev/IProcessReader.java
@@ -0,0 +1,7 @@
+package odev;
+
+public interface IProcessReader {
+
+	// Yeni gelecek olan process olup olmadigini dondurur.
+	boolean isEmpty();
+}
---
+++ Grup_31_/src/odev/Program.java
@@ -0,0 +1,9 @@
+package odev;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	}
+}
+
